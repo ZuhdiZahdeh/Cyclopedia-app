b9f9e6e1d5e2350775c3a09917bde9b924386669
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-06-18 11:23:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,21 +6,3 @@ window.addEventListener("DOMContentLoaded", () => {
   loadLanguage(lang);
 });
 
-
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
